0db3ec088f7712697d41c25e8367c008f78298de
Sharad Agarwal <sagarwal@microsoft.com>
2017-08-07 16:43:13 -0700

    copy from VSO

---
+++ SocialPlus.Client/Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SocialPlus.Client.Models")]
+[assembly: AssemblyDescription("Library Models for communicating with the MSR Embedded Social SDK Service API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SocialPlus.Client.Models")]
+[assembly: AssemblyCopyright("Copyright (c) 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b19914f3-ff67-4d80-bc7a-6d0cbb740d3a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.7.1.1")]
+[assembly: AssemblyFileVersion("0.7.1.1")]
